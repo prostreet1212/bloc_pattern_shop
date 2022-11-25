fb314859a9d8edfe60bb3e2ed4c29cc0060bf5e6
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2022-11-26 00:24:20 +0300

    123

--- lib/blocs/menu_bloc.dart
+++ lib/blocs/menu_bloc.dart
@@ -5,27 +5,30 @@ import 'package:bloc_pattern_shop/states/app_state.dart';
 
 import '../model/coffee_menu.dart';
 
-
 /*class MarketingEvent {
   void buyNot(){
 
   }
  }*/
-abstract class Action{}
-abstract class MarketingAction extends Action{}
+abstract class Action {}
+
+abstract class MarketingAction extends Action {}
+
 class BuyNot extends MarketingAction {
   final CoffeeMenu coffeeMenu;
+
   BuyNot(this.coffeeMenu);
 }
 
 class MarketingBloc {
-
-  AppState appState=AppState(ConstMenuRepository().listMenu, []);
+  AppState appState =
+      AppState(menuList: ConstMenuRepository().listMenu, badgeList: []);
 
   final _stateController = StreamController<AppState>();
   final _eventsController = StreamController<Action>();
 
-  Stream<AppState> get state=>_stateController.stream;
+  Stream<AppState> get state => _stateController.stream;
+
   Sink<Action> get action => _eventsController.sink;
 
   MarketingBloc() {
@@ -38,21 +41,19 @@ class MarketingBloc {
   }
 
   void _handleEvent(Action action) async {
-      //menuList.add(CoffeeMenu('image1', 'name1', 4));
-    if(action is BuyNot){
-     List<CoffeeMenu?> list=appState.menuList.map((e) {
-       if(e==action.coffeeMenu){
-         e.isBuy=!action.coffeeMenu.isBuy;
-       }else{
-         return e;
-       }
-     }).toList();
-     appState=appState.copyWith(list,appState.badgeList);
+    //menuList.add(CoffeeMenu('image1', 'name1', 4));
+    if (action is BuyNot) {
+      List<CoffeeMenu> list = appState.menuList.map((e) {
+        if(e==action.coffeeMenu){
+          e.isBuy=!e.isBuy;
+          return e;
+        }else{
+          return e;
+        }
+      } ).toList();
+      appState = appState.copyWith(
+          menuList: list, badgeList: appState.badgeList);
     }
-
     _stateController.add(appState);
-
   }
 }
-
-
--- lib/states/app_state.dart
+++ lib/states/app_state.dart
@@ -4,12 +4,27 @@ class AppState{
   List<CoffeeMenu> menuList;
 List<CoffeeMenu> badgeList;
 
-  AppState(this.menuList, this.badgeList);
+  AppState({ this.menuList=const[],  this.badgeList=const[]});
 
-  AppState copyWith({required List<CoffeeMenu>? menuList, required List<CoffeeMenu>? badgeList}) {
+  AppState copyWith({required List<CoffeeMenu>? menuList,required List<CoffeeMenu>? badgeList}) {
     return AppState(
-      menuList:menuList ?? this.menuList,
+
+     menuList: menuList  ?? this.menuList,
       badgeList:badgeList ?? this.badgeList,
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is AppState &&
+              runtimeType == other.runtimeType &&
+              menuList == other.menuList &&
+              badgeList == other.badgeList;
+
+  @override
+  int get hashCode => menuList.hashCode ^ badgeList.hashCode;
+
+  @override
+  String toString() => 'SpeakersState{speakers: $menuList, filter: $badgeList}';
 }
